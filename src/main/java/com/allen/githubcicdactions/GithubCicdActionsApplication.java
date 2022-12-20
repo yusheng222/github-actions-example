52689797fd1387a3e515c141594ff25bdc75d3a8
allen <yushengchen1996@163.com>
2022-12-20 11:12:01 +0800

    code

---
+++ src/main/java/com/allen/githubcicdactions/GithubCicdActionsApplication.java
@@ -0,0 +1,20 @@
+package com.allen.githubcicdactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubCicdActionsApplication {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "welcome bitch!!!!";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(GithubCicdActionsApplication.class, args);
+    }
+
+}
